e4c0de8fa937c8d3afea2b52b77552d265e679dd
Alexandru-Paul Dumitru <alexandru.dumitru@broadcom.com>
2020-06-29 16:29:11 +0200

    add Factory example

--- Creational_Design_Patterns/Factory/README.md
+++ Creational_Design_Patterns/Factory/README.md
@@ -1,1 +1,3 @@
+- it is a combination of the Single Responsibility and Open/Closed Principles
+
 > [BACK TO MENU](../README.md)
---
+++ Creational_Design_Patterns/Factory/app.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+var game_characters_factory_1 = require("./game-characters-factory");
+var warrior = game_characters_factory_1.GameCharactersFactory.getWarrior(6);
+var mage = game_characters_factory_1.GameCharactersFactory.getMage(12);
+console.log("Warrior at level 6:", warrior);
+console.log("Mage at level 12:", mage);
